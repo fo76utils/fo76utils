259860a2475219dac8d96a83f89619ae05ef329e
fo76utils <87907510+fo76utils@users.noreply.github.com>
2022-04-10 23:03:12 +0200

    Allow specifying the view transform, for use with render

--- src/markers.cpp
+++ src/markers.cpp
@@ -3,6 +3,7 @@
 #include "filebuf.hpp"
 #include "esmfile.hpp"
 #include "ddstxt.hpp"
+#include "nif_file.hpp"
 
 static void createIcon256(std::vector< unsigned char >& buf, unsigned int c)
 {
@@ -282,9 +283,7 @@ class MapImage : public ESMFile
   std::vector< unsigned int > buf;
   size_t  imageWidth;
   size_t  imageHeight;
-  float   xOffset;
-  float   yOffset;
-  float   xyScale;
+  NIFFile::NIFVertexTransform viewTransform;
   unsigned int  priorityMask;
   unsigned int  worldFormID;
   std::set< unsigned int >  formIDs;
@@ -307,11 +306,11 @@ class MapImage : public ESMFile
   void findDisabledMarkers(const ESMRecord& r);
  public:
   MapImage(const char *esmFileName, const std::vector< MarkerDef >& m,
-           int w, int h, int xMin, int yMax, int cellSize, bool isFO76);
+           int w, int h, const NIFFile::NIFVertexTransform& vt);
   virtual ~MapImage();
   const ESMRecord *getParentCell(unsigned int formID) const;
   bool getREFRRecord(REFRRecord& r, unsigned int formID);
-  void drawIcon(size_t n, float x, float y);
+  void drawIcon(size_t n, float x, float y, float z);
   void findMarkers(unsigned int worldID = 0U);
   inline const std::vector< unsigned int >& getImageData() const
   {
@@ -444,25 +443,16 @@ void MapImage::findDisabledMarkers(const ESMRecord& r)
 }
 
 MapImage::MapImage(const char *esmFileName, const std::vector< MarkerDef >& m,
-                   int w, int h, int xMin, int yMax, int cellSize, bool isFO76)
+                   int w, int h, const NIFFile::NIFVertexTransform& vt)
   : ESMFile(esmFileName),
-    markerDefs(m)
+    markerDefs(m),
+    buf(size_t(w) * size_t(h), 0U),
+    imageWidth(size_t(w)),
+    imageHeight(size_t(h)),
+    viewTransform(vt),
+    priorityMask(0U),
+    worldFormID(0U)
 {
-  imageWidth = size_t(w);
-  imageHeight = size_t(h);
-  buf.resize(imageWidth * imageHeight, 0U);
-  int     x0 = xMin * 4096;
-  int     y1 = (yMax + 1) * 4096;
-  if (isFO76)
-  {
-    x0 = x0 - 2048;
-    y1 = y1 - 2048;
-  }
-  xyScale = float(cellSize) / 4096.0f;
-  xOffset = float(-x0) * xyScale;
-  yOffset = float(y1) * xyScale - 1.0f;
-  priorityMask = 0;
-  worldFormID = 0;
   for (size_t i = 0; i < markerDefs.size(); i++)
   {
     if (formIDs.find(markerDefs[i].formID) == formIDs.end())
@@ -568,15 +558,16 @@ bool MapImage::getREFRRecord(REFRRecord& r, unsigned int formID)
   return true;
 }
 
-void MapImage::drawIcon(size_t n, float x, float y)
+void MapImage::drawIcon(size_t n, float x, float y, float z)
 {
   if (n >= markerDefs.size())
     return;
   const MarkerDef&  m = markerDefs[n];
   int     mipLevelI = int(m.mipLevel);
   float   mipMult = float(std::pow(2.0, m.mipLevel - float(mipLevelI)));
-  float   xf = x * xyScale + xOffset - m.xOffs;
-  float   yf = (-y) * xyScale + yOffset - m.yOffs;
+  viewTransform.transformXYZ(x, y, z);
+  float   xf = x - m.xOffs;
+  float   yf = y - m.yOffs;
   int     xi = int(xf) - int(xf < 0.0f);
   int     yi = int(yf) - int(yf < 0.0f);
   xf = float(xi) - xf;
@@ -760,7 +751,7 @@ void MapImage::findMarkers(unsigned int worldID)
             continue;
           }
         }
-        drawIcon(size_t(m - markerDefs.begin()), refr.x, refr.y);
+        drawIcon(size_t(m - markerDefs.begin()), refr.x, refr.y, refr.z);
       }
     }
   }
@@ -768,20 +759,27 @@ void MapImage::findMarkers(unsigned int worldID)
 
 static const char *usageStrings[] =
 {
-  "Usage: markers INFILE.ESM[,...] OUTFILE.DDS LANDFILE.DDS ICONLIST.TXT "
+  "Usage:",
+  "    markers INFILE.ESM[,...] OUTFILE.DDS LANDFILE.DDS ICONLIST.TXT "
+  "[worldID]",
+  "    markers INFILE.ESM[,...] OUTFILE.DDS VIEWTRANSFORM ICONLIST.TXT "
   "[worldID]",
   "",
-  "LANDFILE.DDS is the output of fo4land or btddump. The format of "
-  "ICONLIST.TXT",
-  "is a tab separated list of form ID, marker type, icon file, icon mip level",
-  "(0.0 = full size, 1.0 = half size, etc.), and an optional priority (0-15).",
+  "LANDFILE.DDS is the output of fo4land or btddump, to be used as reference",
+  "for image size and mapping coordinates. Alternatively, a comma separated",
+  "list of image width, height, view scale, X, Y, Z rotation, and X, Y, Z",
+  "offsets can be specified, these parameters are used similarly to render.",
+  "",
+  "The format of ICONLIST.TXT is a tab separated list of form ID, marker",
+  "type, icon file, icon mip level (0.0 = full size, 1.0 = half size, etc.),",
+  "and an optional priority (0-15).",
   "",
   "Form ID is the NAME field of the reference. Marker type is the TNAM field",
   "of the reference if it is a map marker (form ID = 0x00000010), or -1 for",
   "any type of map marker. For other object types, if not 0 or -1, it can be",
   "1 or 2 to show references from interior or exterior cells only.",
   "",
-  "The icon file name must be either a BC3/DXT5 compressed DDS texture, or a",
+  "The icon file must be either a DDS texture in a supported format, or a",
   "32-bit integer defining a basic shape and color for a 256x256 icon in",
   "0xTARRGGBB format. A is the opacity, T is the type as shape + edges:",
   "  shape: 1: circle, 5: square, 9: diamond",
@@ -801,29 +799,109 @@ int main(int argc, char **argv)
   std::vector< MarkerDef >  markerDefs;
   try
   {
-    unsigned int  hdrBuf[11];
     int     imageWidth = 0;
     int     imageHeight = 0;
-    int     pixelFormat = 0;
-    DDSInputFile  landFile(argv[3],
-                           imageWidth, imageHeight, pixelFormat, hdrBuf);
-    // "FO", "LAND"
-    if ((hdrBuf[0] & 0xFFFF) != 0x4F46 || hdrBuf[1] != 0x444E414C)
-      throw errorMessage("invalid landscape image file");
-    int     xMin = uint32ToSigned(hdrBuf[2]);
-    int     yMax = uint32ToSigned(hdrBuf[6]);
-    int     cellSize = int(hdrBuf[9]);
-    bool    isFO76 = (hdrBuf[0] == 0x36374F46);
-    unsigned int  worldID = (!isFO76 ? 0x0000003C : 0x0025DA15);
+    float   viewScale = 1.0f;
+    float   viewRotationX = float(std::atan(1.0) * 4.0);
+    float   viewRotationY = 0.0f;
+    float   viewRotationZ = 0.0f;
+    float   viewOffsX = 0.0f;
+    float   viewOffsY = 0.0f;
+    float   viewOffsZ = 0.0f;
+    {
+      const char  *s = argv[3];
+      size_t  n = std::strlen(s);
+      if ((n >= 5 && (FileBuffer::readUInt32Fast(s + (n - 4)) | 0x20202000U)
+                     == 0x7364642EU) ||         // ".dds"
+          !std::strchr(s, ','))
+      {
+        unsigned int  hdrBuf[11];
+        int     pixelFormat = 0;
+        DDSInputFile  landFile(s, imageWidth, imageHeight, pixelFormat, hdrBuf);
+        // "FO", "LAND"
+        if ((hdrBuf[0] & 0xFFFF) != 0x4F46 || hdrBuf[1] != 0x444E414C)
+          throw errorMessage("invalid landscape image file");
+        int     xMin = uint32ToSigned(hdrBuf[2]);
+        int     yMax = uint32ToSigned(hdrBuf[6]);
+        int     cellSize = int(hdrBuf[9]);
+        bool    isFO76 = (hdrBuf[0] == 0x36374F46);
+        int     x0 = xMin * 4096;
+        int     y1 = (yMax + 1) * 4096;
+        if (isFO76)
+        {
+          x0 = x0 - 2048;
+          y1 = y1 - 2048;
+        }
+        viewScale = float(cellSize) / 4096.0f;
+        viewOffsX = float(-x0) * viewScale;
+        viewOffsY = float(y1) * viewScale - 1.0f;
+      }
+      else
+      {
+        for (int i = 0; i < 9; i++)
+        {
+          long    tmp1 = 0L;
+          double  tmp2 = 0.0;
+          char    *endp = (char *) 0;
+          if (i < 2)
+            tmp1 = std::strtol(s, &endp, 0);
+          else
+            tmp2 = std::strtod(s, &endp);
+          if (!endp || endp == s || *endp != (i < 8 ? ',' : '\0'))
+          {
+            throw errorMessage("invalid view transform, "
+                               "must be 9 comma separated numbers");
+          }
+          if (i < 2 && (tmp1 < 1L || tmp1 > 65536L))
+            throw errorMessage("invalid image dimensions");
+          if (i == 2 && !(tmp2 >= (1.0 / 512.0) && tmp2 <= 16.0))
+            throw errorMessage("invalid view scale");
+          if (i >= 3 && i < 6)
+          {
+            if (!(tmp2 >= -360.0 && tmp2 <= 360.0))
+              throw errorMessage("invalid view rotation");
+            tmp2 = tmp2 * (std::atan(1.0) / 45.0);
+          }
+          if (i >= 6 && !(tmp2 >= -1048576.0 && tmp2 <= 1048576.0))
+            throw errorMessage("invalid view offset");
+          if (i < 8)
+            s = endp + 1;
+          if (i == 0)
+            imageWidth = int(tmp1);
+          else if (i == 1)
+            imageHeight = int(tmp1);
+          else if (i == 2)
+            viewScale = float(tmp2);
+          else if (i == 3)
+            viewRotationX = float(tmp2);
+          else if (i == 4)
+            viewRotationY = float(tmp2);
+          else if (i == 5)
+            viewRotationZ = float(tmp2);
+          else if (i == 6)
+            viewOffsX = float(tmp2) + (float(imageWidth) * 0.5f);
+          else if (i == 7)
+            viewOffsY = float(tmp2) + (float(imageHeight) * 0.5f);
+          else if (i == 8)
+            viewOffsZ = float(tmp2);
+        }
+      }
+    }
+    loadTextures(markerDefs, argv[4]);
+
+    MapImage  mapImage(argv[1], markerDefs, imageWidth, imageHeight,
+                       NIFFile::NIFVertexTransform(
+                           viewScale,
+                           viewRotationX, viewRotationY, viewRotationZ,
+                           viewOffsX, viewOffsY, viewOffsZ));
+    unsigned int  worldID = 0U;
     if (argc > 5)
     {
       worldID = (unsigned int) parseInteger(argv[5], 0, "invalid world form ID",
                                             0L, 0x0FFFFFFFL);
     }
-    loadTextures(markerDefs, argv[4]);
-
-    MapImage  mapImage(argv[1], markerDefs, imageWidth, imageHeight,
-                       xMin, yMax, cellSize, isFO76);
+    if (!worldID)
+      worldID = (mapImage.getESMVersion() < 0xC0U ? 0x0000003C : 0x0025DA15);
     mapImage.findMarkers(worldID);
 
     DDSOutputFile outFile(argv[2], imageWidth, imageHeight,
