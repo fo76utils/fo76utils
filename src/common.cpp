4b289d5715768f27d4cfbcc9e0c2ce4eb4de77b8
fo76utils <87907510+fo76utils@users.noreply.github.com>
2022-09-05 22:19:14 +0200

    Fixed rounding in conversion to FP16

--- src/common.cpp
+++ src/common.cpp
@@ -14,21 +14,25 @@ std::runtime_error errorMessage(const char *fmt, ...)
 
 std::uint16_t convertToFloat16(float x)
 {
-#if defined(__GNUC__) && (defined(__x86_64__) || defined(__x86_64))
-  std::uint64_t n;
-  __asm__ ("vcvtss2si %1, %0" : "=r" (n) : "x" (x * float(0x01000000)));
-  std::uint64_t s = 0ULL - (n >> 63);
-  n = (n ^ s) - s;
-  s = s & 0x8000U;
-  if (n < 0x0800U)              // zero or denormal
-    return (std::uint16_t) (s | n);
-  std::uint64_t e = 0;
-  __asm__ ("bsr %1, %0" : "+r" (e) : "r" (n) : "cc");
-  n = ((n >> (e - 11U)) + 1U) >> 1;
-  e = e - 10U;
-  n = (n & 0x0FFFU) + (e << 10);
+#if defined(__i386__) || defined(__x86_64__) || defined(__x86_64)
+  union
+  {
+    float   f;
+    std::uint32_t i;
+  }
+  tmp;
+  tmp.f = x;
+  std::uint32_t n = tmp.i;
+  std::uint32_t s = (n & 0x80000000U) >> 16;
+  n = n & 0x7FFFFFFFU;
+  if (n < 0x39000000U)                  // zero or denormal
+  {
+    tmp.i = n;
+    return std::uint16_t(std::uint32_t(roundFloat(tmp.f * float(1 << 24))) | s);
+  }
+  n = (n - 0x37FFF000U) >> 13;
   n = (n < 0x7FFFU ? n : 0x7FFFU);
-  return (std::uint16_t) (s | n);
+  return std::uint16_t(n | s);
 #else
   int     e = 0;
   int     m = roundFloat(float(std::frexp(x, &e)) * 2048.0f);
@@ -38,8 +42,8 @@ std::uint16_t convertToFloat16(float x)
   m = std::abs(m);
   e = e + 14 + (m >> 11);
   if (e <= 0 || e > 31)
-    return (std::uint16_t) (s | (e <= 0 ? 0x0000 : 0x7FFF));
-  return (std::uint16_t) (s | (e << 10) | (m & 0x03FF));
+    return std::uint16_t(s | (e <= 0 ? 0x0000 : 0x7FFF));
+  return std::uint16_t(s | (e << 10) | (m & 0x03FF));
 #endif
 }
 
