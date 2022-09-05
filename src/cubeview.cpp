d42ce4f709ab40bdaea32a7d7d18c6a6d880a86b
fo76utils <87907510+fo76utils@users.noreply.github.com>
2022-09-05 17:16:55 +0200

    Use SDLDisplay, rotation fix, multi-threading and mouse support

--- src/cubeview.cpp
+++ src/cubeview.cpp
@@ -3,9 +3,35 @@
 #include "nif_file.hpp"
 #include "ba2file.hpp"
 #include "ddstxt.hpp"
+#include "sdlvideo.hpp"
 
-#ifdef HAVE_SDL2
-#  include <SDL2/SDL.h>
+#include <thread>
+
+static void renderCubeMapThread(std::uint32_t *outBuf, int w, int h,
+                                int y0, int y1, const DDSTexture *texture,
+                                const NIFFile::NIFVertexTransform *vt,
+                                float viewScale, float mipLevel, bool isSRGB)
+{
+  outBuf = outBuf + (size_t(y0) * size_t(w));
+  for (int yc = y0; yc < y1; yc++)
+  {
+    for (int xc = 0; xc < w; xc++)
+    {
+      float   x = float(xc) - (float(w) * 0.5f);
+      float   y = viewScale;
+      float   z = (float(h) * 0.5f) - float(yc);
+      vt->rotateXYZ(x, y, z);
+      FloatVector4  c(texture->cubeMap(x, y, z, mipLevel));
+      if (isSRGB)
+      {
+        c *= (1.0f / 255.0f);
+        c.srgbCompress();
+      }
+      outBuf[xc] = (std::uint32_t) c;
+    }
+    outBuf = outBuf + w;
+  }
+}
 
 static void renderCubeMap(const BA2File& ba2File,
                           const std::string& texturePath,
@@ -19,204 +45,175 @@ static void renderCubeMap(const BA2File& ba2File,
     std::fprintf(stderr, "Warning: %s is not a cube map\n",
                  texturePath.c_str());
   }
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0)
-    throw errorMessage("error initializing SDL");
-  bool    enableFullScreen = false;
+  SDLDisplay  display(imageWidth, imageHeight, "cubeview");
+  float   viewRotationX = 0.0f;
+  float   viewRotationY = 0.0f;
+  float   viewRotationZ = 0.0f;
+  int     viewScale = 0;
+  bool    enableGamma = false;
+  std::vector< SDLDisplay::SDLEvent > eventBuf;
+  while (true)
   {
-    SDL_DisplayMode m;
-    if (SDL_GetDesktopDisplayMode(0, &m) == 0)
-      enableFullScreen = (imageWidth == m.w && imageHeight == m.h);
-  }
-  SDL_Window  *sdlWindow = (SDL_Window *) 0;
-  SDL_Surface *sdlScreen = (SDL_Surface *) 0;
-  try
-  {
-    sdlWindow =
-        SDL_CreateWindow("cubeview",
-                         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                         imageWidth, imageHeight,
-                         (enableFullScreen ? SDL_WINDOW_FULLSCREEN : 0));
-    if (!sdlWindow)
-      throw errorMessage("error creating SDL window");
-    sdlScreen = SDL_CreateRGBSurface(0, imageWidth, imageHeight, 32,
-                                     0x000000FFU, 0x0000FF00U,
-                                     0x00FF0000U, 0xFF000000U);
-    if (!sdlScreen)
-      throw errorMessage("error setting SDL video mode");
-    SDL_SetSurfaceBlendMode(sdlScreen, SDL_BLENDMODE_NONE);
-    NIFFile::NIFVertexTransform viewTransform;
-    int     viewScale = 0;
-    bool    enableGamma = false;
-    while (true)
+    NIFFile::NIFVertexTransform viewTransform(
+        1.0f, viewRotationX, viewRotationY, viewRotationZ, 0.0f, 0.0f, 0.0f);
+    NIFFile::NIFVertexTransform viewTransformInv(viewTransform);
+    viewTransformInv.rotateYX = viewTransform.rotateXY;
+    viewTransformInv.rotateZX = viewTransform.rotateXZ;
+    viewTransformInv.rotateXY = viewTransform.rotateYX;
+    viewTransformInv.rotateZY = viewTransform.rotateYZ;
+    viewTransformInv.rotateXZ = viewTransform.rotateZX;
+    viewTransformInv.rotateYZ = viewTransform.rotateZY;
+    float   viewScale_f =
+        float(imageHeight) * float(std::pow(2.0f, float(viewScale) * 0.25f));
+    std::thread *threads[16];
+    int     threadCnt = int(std::thread::hardware_concurrency());
+    threadCnt = (threadCnt > 1 ? (threadCnt < 16 ? threadCnt : 16) : 1);
+    threadCnt = (threadCnt < imageHeight ? threadCnt : imageHeight);
+    std::uint32_t *dstPtr = display.lockDrawSurface();
+    int     y0 = 0;
+    for (int i = 0; i < threadCnt; i++)
     {
-      float   y0 =
-          float(imageHeight) * float(std::pow(2.0f, float(viewScale) * 0.25f));
-      SDL_LockSurface(sdlScreen);
-      for (int yc = 0; yc < imageHeight; yc++)
+      int     y1 = (imageHeight * (i + 1)) / threadCnt;
+      try
       {
-        Uint32  *dstPtr = reinterpret_cast< Uint32 * >(sdlScreen->pixels);
-        dstPtr = dstPtr + (size_t(sdlScreen->pitch >> 2) * size_t(yc));
-        for (int xc = 0; xc < imageWidth; xc++, dstPtr++)
-        {
-          float   x = float(xc - (imageWidth >> 1));
-          float   y = y0;
-          float   z = float((imageHeight >> 1) - yc);
-          viewTransform.rotateXYZ(x, y, z);
-          FloatVector4  c(texture.cubeMap(x, y, z, mipLevel));
-          if (enableGamma)
-          {
-            c *= (1.0f / 255.0f);
-            c.srgbCompress();
-          }
-          *dstPtr = (unsigned int) c;
-        }
+        threads[i] = new std::thread(renderCubeMapThread, dstPtr,
+                                     imageWidth, imageHeight, y0, y1, &texture,
+                                     &viewTransformInv, viewScale_f, mipLevel,
+                                     enableGamma);
       }
-      SDL_UnlockSurface(sdlScreen);
-      SDL_BlitSurface(sdlScreen, (SDL_Rect *) 0,
-                      SDL_GetWindowSurface(sdlWindow), (SDL_Rect *) 0);
-      SDL_UpdateWindowSurface(sdlWindow);
+      catch (...)
       {
-        bool    keyPressed = false;
-        bool    quitFlag = false;
-        while (!(keyPressed || quitFlag))
+        threads[i] = (std::thread *) 0;
+        renderCubeMapThread(dstPtr, imageWidth, imageHeight, y0, y1, &texture,
+                            &viewTransformInv, viewScale_f, mipLevel,
+                            enableGamma);
+      }
+      y0 = y1;
+    }
+    for (int i = 0; i < threadCnt; i++)
+    {
+      if (threads[i])
+      {
+        threads[i]->join();
+        delete threads[i];
+      }
+    }
+    display.unlockDrawSurface();
+    display.blitSurface();
+    bool    redrawFlag = false;
+    bool    quitFlag = false;
+    while (!(redrawFlag || quitFlag))
+    {
+      display.pollEvents(eventBuf, 10, false, true);
+      for (size_t i = 0; i < eventBuf.size(); i++)
+      {
+        int     t = eventBuf[i].type();
+        int     d1 = eventBuf[i].data1();
+        int     d3 = eventBuf[i].data3();
+        int     d4 = eventBuf[i].data4();
+        switch (t)
         {
-          SDL_Delay(10);
-          SDL_Event event;
-          while (SDL_PollEvent(&event))
-          {
-            if (event.type != SDL_KEYDOWN)
+          case SDLDisplay::SDLEventWindow:
+            if (d1 == 0)
+              quitFlag = true;
+            else if (d1 == 1)
+              redrawFlag = true;
+            break;
+          case SDLDisplay::SDLEventKeyRepeat:
+          case SDLDisplay::SDLEventKeyDown:
+            redrawFlag = true;
+            switch (d1)
             {
-              if (event.type == SDL_QUIT)
-                quitFlag = true;
-              continue;
-            }
-            bool    rotateFlag = false;
-            float   viewRotationX = 0.0f;
-            float   viewRotationY = 0.0f;
-            float   viewRotationZ = 0.0f;
-            switch (event.key.keysym.sym)
-            {
-              case SDLK_0:
-              case SDLK_1:
-              case SDLK_2:
-              case SDLK_3:
-              case SDLK_4:
-              case SDLK_5:
-              case SDLK_6:
-              case SDLK_7:
-              case SDLK_8:
-              case SDLK_9:
-                mipLevel = float(int(event.key.keysym.sym - SDLK_0));
+              case '0':
+              case '1':
+              case '2':
+              case '3':
+              case '4':
+              case '5':
+              case '6':
+              case '7':
+              case '8':
+              case '9':
+                mipLevel = float(d1 - '0');
                 break;
-              case SDLK_MINUS:
-              case SDLK_KP_MINUS:
+              case '-':
+              case SDLDisplay::SDLKeySymKPMinus:
                 mipLevel = (mipLevel > 0.125f ? (mipLevel - 0.125f) : 0.0f);
                 break;
-              case SDLK_EQUALS:
-              case SDLK_KP_PLUS:
+              case '=':
+              case SDLDisplay::SDLKeySymKPPlus:
                 mipLevel = (mipLevel < 8.875f ? (mipLevel + 0.125f) : 9.0f);
                 break;
-              case SDLK_a:
-                viewRotationZ = -0.19634954f;           // 11.25 degrees
-                rotateFlag = true;
+              case 'a':
+                viewRotationZ += 0.19634954f;           // 11.25 degrees
                 break;
-              case SDLK_d:
-                viewRotationZ = 0.19634954f;
-                rotateFlag = true;
+              case 'd':
+                viewRotationZ -= 0.19634954f;
                 break;
-              case SDLK_s:
-                viewRotationX = 0.19634954f;
-                rotateFlag = true;
+              case 's':
+                viewRotationX -= 0.19634954f;
                 break;
-              case SDLK_w:
-                viewRotationX = -0.19634954f;
-                rotateFlag = true;
+              case 'w':
+                viewRotationX += 0.19634954f;
                 break;
-              case SDLK_q:
-                viewRotationY = 0.19634954f;
-                rotateFlag = true;
+              case 'q':
+                viewRotationY -= 0.19634954f;
                 break;
-              case SDLK_e:
-                viewRotationY = -0.19634954f;
-                rotateFlag = true;
+              case 'e':
+                viewRotationY += 0.19634954f;
                 break;
-              case SDLK_HOME:
-                viewTransform = NIFFile::NIFVertexTransform();
+              case SDLDisplay::SDLKeySymHome:
+                viewRotationX = 0.0f;
+                viewRotationY = 0.0f;
+                viewRotationZ = 0.0f;
                 viewScale = 0;
                 break;
-              case SDLK_INSERT:
+              case SDLDisplay::SDLKeySymInsert:
                 viewScale += int(viewScale < 16);
                 break;
-              case SDLK_DELETE:
+              case SDLDisplay::SDLKeySymDelete:
                 viewScale -= int(viewScale > -16);
                 break;
-              case SDLK_PAGEUP:
-              case SDLK_PAGEDOWN:
-                enableGamma = (event.key.keysym.sym == SDLK_PAGEUP);
+              case SDLDisplay::SDLKeySymPageUp:
+              case SDLDisplay::SDLKeySymPageDown:
+                enableGamma = (d1 == SDLDisplay::SDLKeySymPageUp);
                 break;
-              case SDLK_ESCAPE:
+              case SDLDisplay::SDLKeySymEscape:
                 quitFlag = true;
                 break;
               default:
-                continue;
+                redrawFlag = false;
+                break;
             }
-            if (rotateFlag)
-            {
-              NIFFile::NIFVertexTransform
-                  vt(1.0f, viewRotationX, viewRotationY, viewRotationZ,
-                     0.0f, 0.0f, 0.0f);
-              vt *= viewTransform;
-              // normalize
-              FloatVector4  v(vt.rotateXX, vt.rotateYX,
-                              vt.rotateZX, vt.rotateXY);
-              float   tmp = v.dotProduct(v);
-              v = FloatVector4(vt.rotateYY, vt.rotateZY,
-                               vt.rotateXZ, vt.rotateYZ);
-              tmp += v.dotProduct(v);
-              tmp += (vt.rotateZZ * vt.rotateZZ);
-              tmp = float(std::sqrt(3.0f / tmp));
-              vt.rotateXX *= tmp;
-              vt.rotateYX *= tmp;
-              vt.rotateZX *= tmp;
-              vt.rotateXY *= tmp;
-              vt.rotateYY *= tmp;
-              vt.rotateZY *= tmp;
-              vt.rotateXZ *= tmp;
-              vt.rotateYZ *= tmp;
-              vt.rotateZZ *= tmp;
-              viewTransform = vt;
-            }
-            keyPressed = true;
-          }
+          case SDLDisplay::SDLEventMouseMotion:
+            viewRotationZ -= ((float(d3) / float(imageHeight)) * 3.14159265f);
+            viewRotationX -= ((float(d4) / float(imageHeight)) * 3.14159265f);
+            redrawFlag = true;
+            break;
         }
-        if (!quitFlag)
-          continue;
       }
-      break;
     }
-    SDL_FreeSurface(sdlScreen);
-    SDL_DestroyWindow(sdlWindow);
-    SDL_Quit();
-  }
-  catch (...)
-  {
-    if (sdlScreen)
-      SDL_FreeSurface(sdlScreen);
-    if (sdlWindow)
-      SDL_DestroyWindow(sdlWindow);
-    SDL_Quit();
-    throw;
+    if (quitFlag)
+      break;
+    if (viewRotationX < -3.14159265f)
+      viewRotationX += 6.28318531f;
+    else if (viewRotationX > 3.14159265f)
+      viewRotationX -= 6.28318531f;
+    if (viewRotationY < -3.14159265f)
+      viewRotationY += 6.28318531f;
+    else if (viewRotationY > 3.14159265f)
+      viewRotationY -= 6.28318531f;
+    if (viewRotationZ < -3.14159265f)
+      viewRotationZ += 6.28318531f;
+    else if (viewRotationZ > 3.14159265f)
+      viewRotationZ -= 6.28318531f;
   }
 }
-#endif
 
 int main(int argc, char **argv)
 {
   try
   {
-#ifndef HAVE_SDL2
-    throw errorMessage("SDL 2 is required");
-#else
     if (argc != 5 && argc != 6)
     {
       std::fprintf(stderr,
@@ -234,7 +231,6 @@ int main(int argc, char **argv)
     BA2File ba2File(argv[3], ".dds");
     renderCubeMap(ba2File, std::string(argv[4]), renderWidth, renderHeight,
                   mipLevel);
-#endif
   }
   catch (std::exception& e)
   {
