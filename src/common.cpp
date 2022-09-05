20392e435e0de8dd35d7f6712b35995e371bb02c
fo76utils <87907510+fo76utils@users.noreply.github.com>
2022-09-05 17:03:03 +0200

    Use integer types from cstdint, improved conversion to 16-bit floats

--- src/common.cpp
+++ src/common.cpp
@@ -12,8 +12,24 @@ std::runtime_error errorMessage(const char *fmt, ...)
   return std::runtime_error(std::string(buf));
 }
 
-unsigned short convertToFloat16(float x)
+std::uint16_t convertToFloat16(float x)
 {
+#if defined(__GNUC__) && (defined(__x86_64__) || defined(__x86_64))
+  std::uint64_t n;
+  __asm__ ("vcvtss2si %1, %0" : "=r" (n) : "x" (x * float(0x01000000)));
+  std::uint64_t s = 0ULL - (n >> 63);
+  n = (n ^ s) - s;
+  s = s & 0x8000U;
+  if (n < 0x0800U)              // zero or denormal
+    return (std::uint16_t) (s | n);
+  std::uint64_t e = 0;
+  __asm__ ("bsr %1, %0" : "+r" (e) : "r" (n) : "cc");
+  n = ((n >> (e - 11U)) + 1U) >> 1;
+  e = e - 10U;
+  n = (n & 0x0FFFU) + (e << 10);
+  n = (n < 0x7FFFU ? n : 0x7FFFU);
+  return (std::uint16_t) (s | n);
+#else
   int     e = 0;
   int     m = roundFloat(float(std::frexp(x, &e)) * 2048.0f);
   if (!m)
@@ -22,8 +38,9 @@ unsigned short convertToFloat16(float x)
   m = std::abs(m);
   e = e + 14 + (m >> 11);
   if (e <= 0 || e > 31)
-    return (unsigned short) (s | (e <= 0 ? 0x0000 : 0x7FFF));
-  return (unsigned short) (s | (e << 10) | (m & 0x03FF));
+    return (std::uint16_t) (s | (e <= 0 ? 0x0000 : 0x7FFF));
+  return (std::uint16_t) (s | (e << 10) | (m & 0x03FF));
+#endif
 }
 
 long parseInteger(const char *s, int base, const char *errMsg,
