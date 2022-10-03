3e629fcddc78a4862d8a78905d7bc508034b9cd3
fo76utils <87907510+fo76utils@users.noreply.github.com>
2022-10-04 00:58:34 +0200

    New polynomials for specular functions

--- src/frtable.cpp
+++ src/frtable.cpp
@@ -267,3 +267,22 @@ const float Plot3D_TriShape::fresnelRoughTable[1024] =
   -1.06313069f,  2.71235990f, -2.64465368f,  0.99542870f        // r = 0.03125
 };
 
+// normalized function for general Fallout 76 materials
+
+const float Plot3D_TriShape::fresnelPoly3N_Glass[4] =
+{
+  -1.14821728f,  2.88290570f, -2.73468842f,  1.00000000f
+};
+
+const float Plot3D_TriShape::fresnelPoly3_Water[4] =
+{
+  -1.30214688f,  3.32294874f, -2.87825095f,  1.00000000f
+};
+
+// fixed function for Fallout 4 specular, refractive index = 1.666667
+
+const float Plot3D_TriShape::fresnelPoly3_FO4[4] =
+{
+  -1.17236654f,  2.95903735f, -2.53667081f,  1.00000000f
+};
+
