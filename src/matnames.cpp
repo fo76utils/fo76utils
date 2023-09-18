9b052a6ae0dd70b32c24f446f55be09d9683bc87
fo76utils <87907510+fo76utils@users.noreply.github.com>
2023-09-18 23:33:44 +0200

    Tables of strings for printing material information

---
+++ src/matnames.cpp
@@ -0,0 +1,37 @@
+
+#ifndef MATNAMES_CPP_INCLUDED
+#define MATNAMES_CPP_INCLUDED
+
+static const char *objectTypeStrings[7] =
+{
+  "", "MaterialFile", "Blender", "Layer", "Material", "TextureSet", "UVStream"
+};
+
+static const char *materialFlagNames[32] =
+{
+  "", "", "Is effect", "Is decal", "Two sided",
+  "Is vegetation", "", "Emissive", "", "",
+  "", "", "", "", "",
+  "", "Is terrain", "Is water", "Alpha testing", "Alpha blending",
+  "", "", "", "", "",
+  "", "", "", "", "",
+  "", ""
+};
+
+static const char *opacityModeNames[4] =
+{
+  "Lerp", "Additive", "Subtractive", "Multiplicative"
+};
+
+static const char *textureAddressModeNames[4] =
+{
+  "Wrap", "Clamp", "Mirror", "Border"
+};
+
+static const char *colorModeNames[2] =
+{
+  "Multiply", "Lerp"
+};
+
+#endif
+
